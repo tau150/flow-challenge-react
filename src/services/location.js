6508f0edc42f0a64cb05431007bef8ac92df8a2e
tau150 <tau150@hotmail.com>
2020-03-14 18:02:20 -0300

    deleted https, not supported in free version

--- src/services/location.js
+++ src/services/location.js
@@ -2,7 +2,7 @@ import request from '../utils/request';
 
 export default {
   fetch: () => {
-    const url = 'https://ip-api.com/json/';
+    const url = 'http://ip-api.com/json/';
     return request(url)
   }
 }
